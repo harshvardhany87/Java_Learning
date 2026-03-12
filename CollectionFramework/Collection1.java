8750db30b1d0aa53de8f969f8bb668143dd5296a
harshvardhany87 <harshvardhany87@gmail.com>
2026-03-12 13:05:33 +0900

    Array list and Vector list

---
+++ CollectionFramework/Collection1.java
@@ -0,0 +1,25 @@
+package CollectionFramework;
+
+import java.util.*;
+
+public class Collection1 {
+    public static void main(String args[]) {
+
+        Vector<String> vector1 = new Vector<>();
+        vector1.add("10");
+        vector1.add("22");
+        vector1.add("41");
+
+        ArrayList<String> list1 = new ArrayList<>(5);
+        list1.add("Sam");
+        list1.add("John");
+        list1.add("Josh");
+        list1.add("Noah");
+
+        list1.addAll(vector1);
+
+        System.out.println("The list is: " + list1);
+
+    }
+
+}
