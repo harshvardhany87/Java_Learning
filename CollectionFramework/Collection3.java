90d19cb8e1baf93012f56684bcc68ffa4e7aacee
harshvardhany87 <harshvardhany87@gmail.com>
2026-03-13 14:05:59 +0900

    HashSet and its Methods

---
+++ CollectionFramework/Collection3.java
@@ -0,0 +1,19 @@
+package CollectionFramework;
+
+// Hash set has total of 8 methods/ functions: add(), remove(), contains(), size(), isEmpty(), clear(), iterator(), clone(). 
+
+import java.util.*;
+
+public class Collection3 {
+    public static void main(String args[]) {
+
+        // Hash Set
+        HashSet<Integer> H1 = new HashSet<>();
+        H1.add(30);
+        H1.add(100);
+        H1.add(40);
+        H1.add(20);
+        System.out.println("Hash set: " + H1);
+
+    }
+}
