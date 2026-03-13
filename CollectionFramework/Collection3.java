b7a99cb0c0a89f7f7b541df7b9f7bb3bf79aefd7
harshvardhany87 <harshvardhany87@gmail.com>
2026-03-13 16:28:51 +0900

    Navigable set and its Methods

--- CollectionFramework/Collection3.java
+++ CollectionFramework/Collection3.java
@@ -1,10 +1,12 @@
-package CollectionFramework;
+//package CollectionFramework;
 
 // Hash set has total of 8 methods/ functions: add(), remove(), contains(), size(), isEmpty(), clear(), iterator(), clone(). 
 
 // LinkedHash  set has total of 8 methods/ functions: add(), remove(), contains(), size(), isEmpty(), clear(), iterator(), clone(). 
 
-//Sorted Set has total for 6 methods/ fucntions: first(), last(), headSet(), tailSet(), subSet(), comparator().
+// Sorted Set has total of 6 methods/ fucntions: first(), last(), headSet(), tailSet(), subSet(), comparator().
+
+// Navigable Set has total of 7 mathods: ceiling(), floor(), higher(), lower(), pollFirst(), pollLast(), descendingSet().
 
 import java.util.*;
 
@@ -39,6 +41,15 @@ public class Collection3 {
         System.out.println(T1.headSet(40)); // returns the value from starting point to n-1
         System.out.println(T1.tailSet(20)); // returns the value from the element stated
         System.out.println(T1.subSet(20, 40)); // returns the value from stated to n-1
+
+        System.out.println(T1.ceiling(32)); // returns the value of greater than or equal to, to the stated element
+        System.out.println(T1.floor(31));// returns the value of smaller than or equal to, to the stated element
+        System.out.println(T1.higher(30));// returns the value of greater than the stated element
+        System.out.println(T1.lower(30));// returns the value of smaller than the stated element
+        System.out.println(T1.pollFirst());// return the value after removing the very first element from the list
+        System.out.println("Sorted Set: " + T1);
+        System.out.println(T1.pollLast());// return the value after removing the very last element from the list
+        System.out.println("Sorted Set: " + T1);
 
     }
 }
