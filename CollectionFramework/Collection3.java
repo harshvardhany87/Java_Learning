ad8f47e0993f693e886121024c4f9e88903d895e
harshvardhany87 <harshvardhany87@gmail.com>
2026-03-13 14:06:28 +0900

    LinkedHash Set and its methods

--- CollectionFramework/Collection3.java
+++ CollectionFramework/Collection3.java
@@ -2,6 +2,8 @@ package CollectionFramework;
 
 // Hash set has total of 8 methods/ functions: add(), remove(), contains(), size(), isEmpty(), clear(), iterator(), clone(). 
 
+// LinkedHash  set has total of 8 methods/ functions: add(), remove(), contains(), size(), isEmpty(), clear(), iterator(), clone(). 
+
 import java.util.*;
 
 public class Collection3 {
@@ -14,6 +16,14 @@ public class Collection3 {
         H1.add(40);
         H1.add(20);
         System.out.println("Hash set: " + H1);
+
+        // LinkedHash Set
+        LinkedHashSet<Integer> LH1 = new LinkedHashSet<>();
+        LH1.add(30);
+        LH1.add(10);
+        LH1.add(40);
+        LH1.add(20);
+        System.out.println("LinkedHash set: " + LH1);
 
     }
 }
