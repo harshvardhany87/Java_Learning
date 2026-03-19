b37b688133071ce8cfcc9054e10e5b663e64bd49
harshvardhany87 <harshvardhany87@gmail.com>
2026-03-19 21:07:28 +0900

    Hash set and Arrays practice question

--- CollectionFramework/Prac.java
+++ CollectionFramework/Prac.java
@@ -196,6 +196,19 @@ public class Prac {
          * System.out.println("Unique elements: " + unique);
          */
 
+        /*
+         * Q13
+         * Integer array[] = { 22, 3, 32, 6, 26 };
+         * int max = array[0];
+         * 
+         * for (int i = 0; i < array.length; i++) {
+         * if (array[i] > max) {
+         * max = array[i];
+         * }
+         * }
+         * System.out.println("Largest number is: " + max);
+         */
+
     }
 
 }
