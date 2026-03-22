c6e357634206b4d456f65081ba8a690f0fbb795a
harshvardhany87 <harshvardhany87@gmail.com>
2026-03-22 12:34:24 +0900

    Sliding Window using Queue

---
+++ Practice/SlidingWindowMax.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class SlidingWindowMax {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] result = new int[nums.length - k + 1];
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // Remove out-of-window elements
+            if (!dq.isEmpty() && dq.peek() == i - k) {
+                dq.poll();
+            }
+
+            // Remove smaller elements
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+
+            dq.offer(i);
+
+            // Store result
+            if (i >= k - 1) {
+                result[index++] = nums[dq.peek()];
+            }
+        }
+
+        return result;
+    }
+}
